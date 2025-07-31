ef89a00b5c4686365ebe1f62d5d90acd8b124fbd
architchatur <architc@berkeley.edu>
2025-07-30 20:33:47 -0700

    Update test_login_helper.py

--- test_login_helper.py
+++ test_login_helper.py
@@ -1,14 +1,15 @@
 from login_helper import is_valid_username, is_valid_password
 
 def test_valid_username():
-    assert is_valid_username('test_user')
-    assert not is_valid_username("hi")
-    assert not is_valid_username("abcde")
+    assert is_valid_username('testuser123')  
+    assert not is_valid_username("hi")       
+    assert not is_valid_username("abcde")   
+    assert not is_valid_username("test_user") 
 
 def test_valid_password():
-    assert is_valid_password('ABCDEFGHIJK')
-    assert not is_valid_password('PASSWRD')
-
+    assert is_valid_password('ABCDEFGHIJK')   
+    assert not is_valid_password('PASSWRD')  
+    
 def test_regression_inputs():
-    assert is_valid_username("user1234")
-    assert is_valid_password("PASSWORD")
+    assert is_valid_username("user1234")      
+    assert is_valid_password("PASSWORD")     
